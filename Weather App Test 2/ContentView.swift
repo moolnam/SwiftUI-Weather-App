5fb704b0bc762f43ba7dfd0db32b389e71e2ed14
moolnam <moolnam>
2022-05-08 16:39:50 +0900

    Initial Commit

---
+++ Weather App Test 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather App Test 2
+//
+//  Created by KimJongHee on 2022/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather App Test 2/Weather_App_Test_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_Test_2App.swift
+//  Weather App Test 2
+//
+//  Created by KimJongHee on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_Test_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
